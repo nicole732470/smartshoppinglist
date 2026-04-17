d0772f6fdf5195ba92fef3457b0e3d9a69bd8157
nicole <nicole732470@gmail.com>
2026-04-17 16:14:40 -0500

    Add model validations and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
-end
+    has_many :price_records, dependent: :destroy
+  
+    validates :name, presence: true
+    validates :category, presence: true
+  
+    def lowest_price
+      price_records.minimum(:price)
+    end
+  
+    def latest_price
+      price_records.order(recorded_at: :desc).first&.price
+    end
+  
+    def latest_store
+      price_records.order(recorded_at: :desc).first&.store_name
+    end
+  end
